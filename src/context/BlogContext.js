a121ead23190b3f209abc08110f69b2370770715
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-20 22:15:05 +0200

    Rendering a List of Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,9 +3,17 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+
+    const blogPosts = [
+        { title: 'Blog post #1' },
+        { title: 'Blog post #2' }
+    ]
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={blogPosts}>
             { children }
         </BlogContext.Provider>
     )
 };
+
+export default BlogContext;
