a5bdcbd7b91aa6fc0774fccf538311d3025e1203
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-21 09:42:59 +0200

    Add blogpost to Index onSubmit

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
@@ -23,9 +23,7 @@ const BlogPostForm = () => {
 
             <Button 
                 title="Save Blog Post" 
-                onPress={() => {
-                    
-                }}
+                onPress={() => onSubmit(title, content)}
             />
         </View>
     );
