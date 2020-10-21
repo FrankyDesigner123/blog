86f64d3ae8c302921a396d3eec98c4ba94a31a05
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-21 09:36:53 +0200

    Add BlogPostForm component

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+
+const BlogPostForm = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    return(
+        <View>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput 
+                value={title}
+                onChangeText={(text) => setTitle(text)}
+                style={styles.inputField} 
+            />
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput 
+                value={content}
+                onChangeText={(content) => setContent(content)}
+                style={styles.inputField} 
+            />
+
+            <Button 
+                title="Save Blog Post" 
+                onPress={() => {
+                    
+                }}
+            />
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    inputField: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5,
+    }
+});
+
+export default BlogPostForm;
